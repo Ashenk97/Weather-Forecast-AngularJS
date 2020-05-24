493aaca76c2c26995dd1a0cade7237eab78524b9
Ashen Kavinda <49136460+Ashenk97@users.noreply.github.com>
2020-05-24 21:51:34 +0530

    Updated tweaks

--- app/controllers.js
+++ app/controllers.js
@@ -1,34 +1,43 @@
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
-    
+weatherApp.controller("homeController", [
+  "$scope",
+  "cityService",
+  function ($scope, cityService) {
     $scope.city = cityService.city;
-    
-    $scope.$watch('city', function() {
-       cityService.city = $scope.city; 
-    });
-    
-}]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
-    
+    $scope.$watch("city", function () {
+      cityService.city = $scope.city;
+    });
+  },
+]);
+
+weatherApp.controller("forecastController", [
+  "$scope",
+  "$resource",
+  "$routeParams",
+  "cityService",
+  function ($scope, $resource, $routeParams, cityService) {
     $scope.city = cityService.city;
-    
-    $scope.days = $routeParams.days || '2';
-    
-    $scope.weatherAPI = $resource("https://api.openweathermap.org/data/2.5/onecall?APPID=7423253aeb102c51c2c100b80c8ad041", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
-    
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
-    
-    $scope.convertToFahrenheit = function(degK) {
-        
-        return Math.round((1.8 * (degK - 273)) + 32);
-        
-    }
-    
-    $scope.convertToDate = function(dt) { 
-      
-        return new Date(dt * 1000);
-        
+
+    $scope.days = $routeParams.days || "2";
+
+    $scope.weatherAPI = $resource(
+      "https://api.openweathermap.org/data/2.5/onecall?APPID=7423253aeb102c51c2c100b80c8ad041",
+      { callback: "JSON_CALLBACK" },
+      { get: { method: "JSONP" } }
+    );
+
+    $scope.weatherResult = $scope.weatherAPI.get({
+      q: $scope.city,
+      cnt: $scope.days,
+    });
+
+    $scope.convertToFahrenheit = function (degK) {
+      return Math.round(1.8 * (degK - 273) + 32);
     };
-    
-}]);
+
+    $scope.convertToDate = function (dt) {
+      return new Date(dt * 1000);
+    };
+  },
+]);
